7df57ad70d7b1072902c5f97bca8444586d70d04
Sebastian <menaloaizasebastian@gmail.com>
2024-06-20 02:23:44 -0500

    admin

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -27,8 +27,11 @@ class AuthenticatedSessionController extends Controller
         $request->authenticate();
 
         $request->session()->regenerate();
-
-        return redirect()->intended(route('dashboard', absolute: false));
+        
+        if($request->user()->usertype == 'admin'){
+            return redirect('admin/dashboard');
+        }
+        return redirect()->intended(route('dashboard'       ));
     }
 
     /**
